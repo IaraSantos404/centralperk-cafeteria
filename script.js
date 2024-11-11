c0331456dcf85db8616e018786826812dca54882
iara <iarasantosdev404@gmail.com>
2024-11-11 18:31:37 -0300

    iniciando carrossel de avaliações

---
+++ script.js
@@ -0,0 +1,26 @@
+const imgs = document.getElementById("carousel-images");
+const img = document.querySelectorAll("#carousel-images .card-carrousel");
+
+let idx = 0;
+let tamanho = 33.33;
+
+function showNextImage() {
+  idx++;
+  if (idx > img.length - 1) {
+    idx = 0; 
+  }
+  updateCarousel();
+}
+
+function showPreviousImage() {
+  idx--;
+  if (idx < 0) {
+    idx = img.length - 1; 
+  }
+  updateCarousel();
+}
+
+
+function updateCarousel() {
+  imgs.style.transform = `translateX(-${(idx * tamanho)}%`; 
+}
